fe06297cb05e109a1e4652c2b3a7f308259f2864
alexanderlebedev1989 <AlexanderLebedev1989@gmail.com>
2020-04-22 18:19:21 +0500

    5.3.1. Создать динамический список на базе массива.[#271878]

--- chapter_001/src/main/java/ru/job4j/collection/SimpleArray.java
+++ chapter_001/src/main/java/ru/job4j/collection/SimpleArray.java
@@ -6,7 +6,6 @@ public class SimpleArray<T> implements Iterable<T> {
 
     private int index = 0;
     private int modCount = 0;
-    private int lastModified = 0;
     private int size = 0;
     private int position = 0;
 
@@ -16,37 +15,45 @@ public class SimpleArray<T> implements Iterable<T> {
         if (index >= size) {
             throw new ArrayIndexOutOfBoundsException();
         }
-            return (T) array[index];
-        }
+        return (T) array[index];
+    }
 
-        public void add(T model) {
-            modCount++;
-            if (index >= size) {
-                array = Arrays.copyOf(array, size + 1);
-            }
-            array[index++] = model;
-            size++;
+    public void add(T model) {
+        modCount++;
+        if (index >= size) {
+            array = Arrays.copyOf(array, size + 1);
+        }
+        array[index++] = model;
+        size++;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new SimpleArrayIterator<T>() {
+        };
+    }
+
+    private class SimpleArrayIterator<T> implements Iterator<T> {
+        private int expectedModCount = modCount;
+
+        @Override
+        public boolean hasNext() {
+            return position != size;
         }
 
         @Override
-        public Iterator<T> iterator() {
-            return new Iterator<T>() {
-                @Override
-                public boolean hasNext() {
-                    return position <= size;
-                }
-
-                @Override
-                public T next() {
-                    if (!hasNext()) {
-                        throw new NoSuchElementException();
-                    }
-                    if (modCount != lastModified) {
-                        throw new ConcurrentModificationException();
-                    }
-                    return (T) array[position++];
-                }
-            };
+        public T next() {
+            checkForComodification();
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            return (T) array[position++];
         }
 
+        final void checkForComodification() {
+            if (modCount != expectedModCount) {
+                throw new ConcurrentModificationException();
+            }
+        }
+    }
 }
--- chapter_001/src/test/java/ru/job4j/collection/SimpleArrayTest.java
+++ chapter_001/src/test/java/ru/job4j/collection/SimpleArrayTest.java
@@ -1,6 +1,5 @@
 package ru.job4j.collection;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.is;
 import org.junit.Test;
