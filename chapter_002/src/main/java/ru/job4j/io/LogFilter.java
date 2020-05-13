29121035b64dc526a908189aac14adc13c123cf0
alexanderlebedev1989 <AlexanderLebedev1989@gmail.com>
2020-05-13 18:20:05 +0500

    0.3. BufferedReader. [#271922]

--- chapter_002/src/main/java/ru/job4j/io/LogFilter.java
+++ chapter_002/src/main/java/ru/job4j/io/LogFilter.java
@@ -6,32 +6,23 @@ import java.util.List;
 
 public class LogFilter {
 
-    public static List<String> filter(String file) {
-        List<String> lines404 = new ArrayList<>();
+    public static List<String> filter(String file)  {
+        List<String> lines = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(file))) {
-            List<String> lines = new ArrayList<>();
-            in.lines().forEach(lines::add);
-            for (String line : lines) {
-                 line = line.replace("-", "-1");
-                 String[] arrayLine = line.split(" ");
-                 int number = Integer.parseInt(arrayLine[arrayLine.length - 2]);
-                 int number1 = Integer.parseInt(arrayLine[arrayLine.length - 1]);
-                 if (number == 404 && number1 != -1) {
-                     line = line.replace("-1", "-");
-                     lines404.add(line);
-                 }
-            }
-        } catch (Exception e) {
+            in.lines().filter(s -> s.contains("404")).
+                    filter(s -> !s.endsWith("-")).forEach(lines::add);
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        return lines404;
+        return lines;
     }
 
     public static void save(List<String> log, String file) {
         try (PrintWriter out = new PrintWriter(
                 new BufferedOutputStream(new FileOutputStream(file)))) {
+
             for (String line : log) {
-                out.write(line + "\n");
+                out.write(line + System.lineSeparator());
                 System.out.println(line);
             }
         } catch (Exception e) {
