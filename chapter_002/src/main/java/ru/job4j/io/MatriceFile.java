9b708f7b4218d6a473777c05553426df78865be1
alexanderlebedev1989 <AlexanderLebedev1989@gmail.com>
2020-05-13 15:44:20 +0500

    0.1. FileOutputStream. [#271921]

--- chapter_002/src/main/java/ru/job4j/io/MatriceFile.java
+++ chapter_002/src/main/java/ru/job4j/io/MatriceFile.java
@@ -3,6 +3,7 @@ package ru.job4j.io;
 import java.io.FileOutputStream;
 
 public class MatriceFile {
+
     public static void main(String[] args) {
         ArrayMatrice am = new ArrayMatrice();
         try (FileOutputStream out = new FileOutputStream("Matrice.txt")) {
